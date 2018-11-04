e414cd8ddb4d8bf44ea1ba34643a5130ea493d17
akhiljayaram <akhilpj@qburst.com>
2018-11-04 22:48:45 +0400

    Added base project workspace with Cocoa pods  - Added Alamofire  - Added Kingfisher

---
+++ NewsApp/NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by b2c on 11/4/18.
+//  Copyright © 2018 b2c. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
